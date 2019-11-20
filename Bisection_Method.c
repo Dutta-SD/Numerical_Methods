609d9aaad1fe8a7abe658de124da873a3562d8fc
Dutta-SD <57990650+Dutta-SD@users.noreply.github.com>
2019-11-20 20:29:05 +0530

    Bisection_Method_Initial_Code
    The code was cleaned, comments added and changes made.

--- Bisection_Method.c
+++ Bisection_Method.c
@@ -1,60 +1,72 @@
-/* C program to implement Bisection Method
-for solving non linear equations
+/*
+C program to implement Bisection Method
+for approximating roots of equations 
 */
 
-/* This method works to find the root by using binary search like
+/* 
+This method works to find the root by using binary search like
 divide and conquer approach. We choose an interval [a, b]
-such that f(a) and f(b) are of opposite sign. Then there is always a root 
-of the eqaution between this interval.
-*/
+such that f(a) and f(b) are of opposite sign. Then there 
+is always a root of the eqaution between this interval.
 
-/*Guess 1 for root: (a+b)/2 = x0
+Guess 1 for root: (a+b)/2 = xi
 Assume f(a) is negative and f(b) is positive
 Guess for 2 root: if f(x0) is negative, 
-then root is in [x0, b] and in[a, x0] if 
+then root is in [xi, b] and in[a, xi] if 
 it is positive
 */
 
-/*Iterations stop after a few iterations or if required precision 
-is encountered . Precision/ Error is Epsilon
+/*
+Iterations stop after a few iterations or if required precision 
+is encountered . Precision/ Error is Epsilon.
+Here precision is not implemented. Loop stops after 30 iterations.
 */
 
 
 //This function is arbritrary. Any function with
-//a real root will do.Chosen function is x^3 - x^2 + x - 1  
+//a real root will do.Chosen function is x^4 - 3*(x^2) + 4*x -1 
 
 #include<stdio.h>
 #include<stdlib.h>
-	
+
+//F takes the variable as input and returns functional values
+//input: double
+//output: double
+
 double F(double x){
     return (x*x*x*x)-(3*x*x)+(4*x)-1;
 }
 
-//Let chosen interval be [-1, 2]. Epsilon = 0.001
+//Epsilon = 0.001 is arbritrary choice. 
+
+//Bisection_Method calculates the approximate roots of F(x)
+//in the given interval.
+//input : double, double
+//output: None
 
 void Bisection_Method(double a, double b){
 	double Epsilon = 0.0001;
-	double xi;
+	double xi;                         //the variable which will have the extra roots
 	int count = 0;
 	
-	xi = (a + b) / 2.0;
+	xi = (a + b) / 2.0;                //Sets initial guess of approx root
+	
 	printf("%lf\n", xi);
 	
-	while( count!=30){
-		if(F(xi) * F(a) > 0){
+	while(count!=30){                  //iterations stop after 30 runs
+		if(F(xi) * F(a) > 0){      //if this condition is true, root is in [a, xi]
 			a = xi;
 			xi = (a+b) / 2.0;
 			count++;
 		}
-		else if(F(xi) * F(a) < 0){
+		else if(F(xi) * F(a) < 0){   //if this condition is true, root is in [xi, b]
 			b = xi;
 			xi = (a+b) / 2.0;
-			//printf("%lf ", xi);
 			count++;
 		}
 			
 	}
-	printf("\nThe solution of the Equation is : %14.13lf", xi);	
+	printf("\nThe solution of the Equation is : %6.3lf", xi);	
 }
 
 int main(){
