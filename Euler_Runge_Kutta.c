2f78ed8b9591ff947176e087721a98c75b057c24
Dutta-SD <57990650+Dutta-SD@users.noreply.github.com>
2019-12-28 01:34:17 +0530

    3 files
    One File has euler method, modified euler method, runge kutta 2nd and 4th order formula. 
    Integral numerical has integration using numerical methods.
    Power method is for finding the largest eigen value of a given matrix.

---
+++ Euler_Runge_Kutta.c
@@ -0,0 +1,71 @@
+/*C program for implementation and comparison of Eulers Method,
+Modified Eulers method, Runge Kutta Method(2nd order and 4th order formula)
+*/
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+/*
+We write
+    y' = f(x, y)
+	Initial Value : (x0, y0)
+*/
+double f(double x, double y) {
+	/* Let us take dy/dx = x*x + y , (0,0) as the initial value */
+	return x*x + y;
+}
+/* We will find out the value in [0, 0.5], step size 0.01 */
+void Euler_Method() {
+	double x0 = 0.0, y0 = 1.0, h = 0.05;
+	int i;
+	printf("\n\tEulers Method for Solving Differential Equations \n");
+	for(i = 0;i < 5; i++) {
+		printf("X : %lf\tY : %lf\n", x0, y0);
+		y0 = y0 + h * f(x0, y0);
+		x0 = x0 + h;
+	}
+}
+
+/*Calculates the value for a particular point. */
+void Modified_Euler(double x0, double y0, double h) {
+	double y1 = y0 + h*f(x0, y0), x1 = x0, temp = 0.0;
+ 	printf("\n\tEulers Modified Method for Solving Differential Equations \n");
+	while(fabs(temp - y1)>0.00000001) {
+		temp = y1;
+		y1 = y0 + ( f(x0, y0) + f(x1, y1) ) * h / 2.0;
+	}
+	printf("X = %lf\tY = %lf\n", x1, y1);
+}
+
+void Runge_Kutta_2nd_Order() {
+	double x0 = 0.05, y0 = 1.0513, h = 0.05, k1, k2;
+	printf("\n\tRunge Kutta 2nd Order Method for Solving Differential Equations \n");
+		k1 = h*f(x0, y0);
+	    k2 = h*(x0 + h, y0 + k1);
+        y0 = y0 + (k1+k2) / 2.0;
+        x0 += h;
+        printf("X : %lf\tY : %lf\n", x0, y0);
+}
+
+void Runge_Kutta_4th_Order() {
+	double x0 = 0.05, y0 = 1.0513, h = 0.05, k1, k2, k3, k4;
+	printf("\n\tRunge Kutta 4th Order Method for Solving Differential Equations \n");
+		k1 = h*f(x0, y0);
+	    k2 = h*(x0 + h/2.0, y0 + k1/2.0);
+	    k3 = h*f(x0 + h/2.0, y0 + k2/2.0);
+	    k4 = h*f(x0 + h, y0 + k3);
+        y0 = y0 + (k1 + 2*k2 + 2*k3 + k4) / 6.0;
+        x0 += h;
+        printf("X : %lf\tY : %lf\n", x0, y0);
+}
+
+
+
+int main() {
+	Euler_Method();
+	Modified_Euler(0.1, 1.0513, 0.05);
+	Runge_Kutta_2nd_Order();
+	Runge_Kutta_4th_Order();
+	return EXIT_SUCCESS;
+}
+
+
---
+++ Power_Method.c
@@ -0,0 +1,170 @@
+/*
+C program to implement power method for finding dominating eigenvalue of a 
+matrix.
+*/
+
+/* 
+An eigen value of a matrix is a nonzero value l such that:
+    AX = lX, where 
+	A is the matrix,
+	X is the eigenvector(non null)
+	l is the eigenvalue.
+	
+A dominating eigen value lp is such that any other eigen value is smaller in 
+magnitude than the magnitude of lp. Let the matrix has eigen values l1, l2,...
+.., ln. A dominating eigen value is such that |lp| > |lj| for j=0, 1, 2; j not 
+equal to p.
+
+This method uses an initial approx for the eigen vector and then computes the 
+next iteraton by multiplying it with the given matrix.The maximum value 
+in the vector is extracted and then it is sent for multiplication again. 
+When this error becomes small, the eigen value computed is the dominating 
+eigen value, and the last computed matrix is the corresponding eigen vector. 
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+/*size is the dimensions of the square matrix.
+Can be changed as per need*/
+
+#define SIZE 3
+
+/*
+Matrix_Input takes the input for the augmented matrix of the system to be solved
+    @Input : mat[SIZE][SIZE], where store the values
+    @Output : None
+*/
+
+void Matrix_Input(double mat[SIZE][SIZE]) {
+	int i, j;
+	printf("ENTER THE MATRIX ELEMENTS[ %d x %d ]:\n", SIZE, SIZE);
+	for(i = 0; i < SIZE; i++) {
+		for(j = 0; j < SIZE; j++) {
+			scanf("%lf", &mat[i][j]);
+		}
+	}
+}
+
+/*
+MatMult is a function that multiplies the two matrices.
+    @Input : A, B, C three matrices to be multiplied. The matrices are of 
+    appropriate dimensions. The result is stored in C. A will be given.
+    B will be previous eigenvector. C will be new eigenvector.
+    @Output : None
+*/
+
+void MatMult(double A[SIZE][SIZE], double B[SIZE][1], double C[SIZE][1]) {
+	int i, j, k;
+	for(i = 0; i < SIZE; i++) {
+		for(j = 0; j < 1; j++) {
+		    C[i][j] = 0;
+			for(k = 0; k < SIZE; k++) {
+				C[i][j] += A[i][k] * B[k][j];
+			}		
+		}		
+	}	
+}
+
+/*Max_Arr computes the maximum value in the given array.
+    @Input : An array whose maximum value needs to be found.
+    @Output : the maximum double value in the arr.
+*/
+
+double Max_Arr(double X[SIZE][1]) {
+	double max;
+	double temp[SIZE];
+	int i;
+	for(i=0;i<SIZE;i++) {
+		temp[i] = fabs(X[i][0]);
+	}
+	max = temp[0];
+	for(i = 0;i < SIZE; i++) {
+		if(temp[i] > max) {
+			max = temp[0];
+		}
+	}
+	for(i = 0;i < SIZE; i++) {
+		if(temp[i] + X[i][0] == 0.0) {
+			return X[i][0];
+		}
+	}
+}
+
+/*
+Divide_by_Eigen divides each element of matrix by eigen value.
+    @Input : X, eigen_value
+	@Output : None
+*/
+
+void Divide_by_Eigen(double X[SIZE][1], double eigen) {
+	int i;
+	for(i = 0;i < SIZE;i++) {
+		X[i][0] = X[i][0] / eigen;
+	}
+}
+
+/*
+Power_method Calculates the Maximum eigen value for the given matrix.
+    @Input : None
+    @Output : None
+*/
+
+void Power_Method() {
+	double A[SIZE][SIZE];
+	int i, j;
+	
+	/*X is the Eigen_value, Xi is a temporary variable.*/
+	
+	double Eigen_value, Eigen_value_prev , X[SIZE][1], Xi[SIZE][1];
+	double epsilon = 0.0001;
+	
+	Matrix_Input(A);            /*Inputted the require matrix*/
+	
+	for(i = 0; i < SIZE; i++){  /*Initialise EigenVector to [1, 1, 1...]*/
+		X[i][0] = 1.0l;
+	}
+	
+	MatMult(A, X, Xi);          /*Xi is temporary variable*/
+    Eigen_value_prev = 0;        /*Random value for Eigenvalue initially*/
+	Eigen_value = Max_Arr(Xi);
+	Divide_by_Eigen(Xi, Eigen_value);
+	
+	
+	for(i = 0; i < SIZE; i++){
+		X[i][0] = Xi[i][0];       /*transfer from temporary to Eigen Vector*/
+	}
+	
+	while(fabs((Eigen_value - Eigen_value_prev)) > epsilon) {
+			Eigen_value_prev = Eigen_value;
+			MatMult(A, X, Xi);
+        	Eigen_value= Max_Arr(Xi);
+        	Divide_by_Eigen(Xi, Eigen_value);
+        	
+	        for(i = 0; i < SIZE; i++){
+		        X[i][0] = Xi[i][0];
+        	}
+	}
+	
+	printf("The eigen value is : %6.3lf ",Eigen_value);
+	printf("\nThe corresponding EigenVector is :\n");
+	for(i = 0;i < SIZE; i++) {
+		printf("%6.3lf ", X[i][0]);
+	}
+ }
+ 
+int main() {
+	Power_Method();
+	return EXIT_SUCCESS;
+}
+
+
+
+
+
+
+
+
+
+
